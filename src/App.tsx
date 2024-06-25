c4994ce0a7ee5f0373e6c6c470141bbf12594165
Sameer Abuzraa <sam@shyp.studio>
2024-06-25 10:56:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is Sameer talking to you!
+    </div>
+  );
+}
+
+export default App;
